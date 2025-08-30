141b8d2844020d1e9e13d003e345f7b7c41910da
Stephen Power <860644+StephenPower@users.noreply.github.com>
2025-08-30 17:50:44 -0400

    updated fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
